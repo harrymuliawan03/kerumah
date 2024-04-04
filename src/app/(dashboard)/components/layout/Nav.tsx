fe89dde1f6887b3fb00fca011a535d7fd6769568
harrymuliawan03 <harrymuliawan03@gmail.com>
2024-04-04 17:18:04 +0700

    adding not-found page

--- src/app/(dashboard)/components/layout/Nav.tsx
+++ src/app/(dashboard)/components/layout/Nav.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Nav: React.FC = () => {
@@ -13,9 +14,9 @@ const Nav: React.FC = () => {
                             <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                             </svg>
                         </button>
-                        <a href="https://flowbite.com" className="flex ms-2 md:me-24">
-                        <Image src="/assets/images/kerumah_logo.png" width={70} height={70} alt="Kerumah Logo" />
-                        </a>
+                        <Link href="/" className="flex ms-2 md:me-24">
+                            <Image src="/assets/images/kerumah_logo.png" width={70} height={70} alt="Kerumah Logo" />
+                        </Link>
                     </div>
                     <div className="flex items-center">
                         <div className="flex items-center ms-3">
@@ -53,7 +54,7 @@ const Nav: React.FC = () => {
                         </div>
                     </div>
                 </div>
-            </nav>
+        </nav>
     )
 }
 
