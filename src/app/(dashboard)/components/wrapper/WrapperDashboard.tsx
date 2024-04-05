c91a68224ed9ca51848db831e0e2de110f8a3274
harrymuliawan03 <harrymuliawan03@gmail.com>
2024-04-05 11:41:55 +0700

    adding component properti page

--- src/app/(dashboard)/components/wrapper/WrapperDashboard.tsx
+++ src/app/(dashboard)/components/wrapper/WrapperDashboard.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const WrapperDashboard: React.FC<Props> = (props) => {
     return (
-        <div className="bg-gray-300">
+        <div className="bg-gray-300 min-h-[100vh]">
             <div className="p-4 sm:ml-64">
                 <div className="p-4 rounded-lg dark:border-gray-700 mt-14">
                     {props.children}
